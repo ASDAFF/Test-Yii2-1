03474f87e0731c64a4698f9cb39110a1bd3b12aa
RomanAngelovskij <turbocom-mlt@yandex.ru>
2015-04-01 10:16:37 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=tests',
-    'username' => 'tests',
-    'password' => 'TestTask',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
